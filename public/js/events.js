699f99166cd35ae1d5c169a9b61645d94b075ca6
Nate Alleyne <ncgalleyne@gmail.com>
2024-03-17 10:08:22 -0400

    print event list

--- public/js/events.js
+++ public/js/events.js
@@ -12,25 +12,13 @@ renderEvents = async () => {
     console.log(data)
     const events = data.upcomingEvents;
     let eventHtml = '';
-    let indicatorsHtml = '<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>';
         events.forEach((event, i) => {
-            if(i === 0)
-                eventHtml += '<div class="carousel-item active">';
-            else{
-                eventHtml += '<div class="carousel-item">';
-                indicatorsHtml += `<li data-target="#carouselExampleIndicators" data-slide-to="${i}"></li>`
-            }
             eventHtml += `
-                <img class="d-block w-100" width="612px" height="450px" src="./img/events/event${i + 1}.jpg" alt="${event.title}">
-                <div class="carousel-caption d-none d-md-block">
-                    <!--<h5 style="color: antiquewhite;">${event.title}</h5>
-                    <p>${event.description}</p>-->
-                </div>
-            </div>
+                    <h5 ><em>${event.title}</em></h5>
+                    <p>${event.description}</p>
                 ` 
         });
         $('.event-inner').html(eventHtml);
-        $('.event-indicators').html(indicatorsHtml);
 }
 
 renderEvents();
