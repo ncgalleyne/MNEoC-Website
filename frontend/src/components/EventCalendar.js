c23d8a2433997b77d1b0fce3073beb81584f2012
Nate Alleyne <ncgalleyne@gmail.com>
2024-03-24 15:27:47 -0400

    add calendar section to homepage

--- frontend/src/components/EventCalendar.js
+++ frontend/src/components/EventCalendar.js
@@ -1,8 +1,10 @@
 // src/components/AddEvent.js
 import React from 'react';
 import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
+import './EventCalendar.css'
 
-const EventCalendar = ({events}) => {
+const EventCalendar = ({events, setSelectedCalendarEvent}) => {
 
   // Function to generate class names for highlighted dates
   const tileClassName = ({ date }) => {
@@ -15,11 +17,25 @@ const EventCalendar = ({events}) => {
     // Return the class name based on whether the current date is highlighted
     return isHighlighted ? 'highlighted-date' : null;
   };
+
+  // Function to handle click on a day
+  const handleClickDay = (date) => {
+    // Find the event for the clicked date
+    const eventForDate = events.find(event =>
+      new Date(event.date).toDateString() === date.toDateString()
+    );
+
+    // If an event exists for the clicked date, set the selectedEvent prop
+    if (eventForDate) {
+      setSelectedCalendarEvent(eventForDate);
+    }
+  };
   
   return (
     <div className="event-calendar-container">
       <Calendar 
         tileClassName={tileClassName}
+        onClickDay={handleClickDay}
       />
     </div>
   );
